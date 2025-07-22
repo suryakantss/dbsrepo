61d82147b4667467b1debfa309da9150205d5160
Suryakant <suryakantsurve@gmail.com>
2025-07-22 10:47:35 +0530

    Language Basics

---
+++ firstjavaproject/src/com/dbs/firstjavaproject/Loops.java
@@ -0,0 +1,18 @@
+package com.dbs.firstjavaproject;
+
+public class Loops {
+    public static void main(String[] args) {
+ var i=0;
+  while (i < 10)
+  {
+      System.out.println("Hello : " + i);
+      i++;
+  }
+        System.out.println("============");
+  for (int j=0;j< 5;j++){
+      System.out.println("Welcome to Java " + j);
+  }
+        System.out.println("==============");
+
+    }
+}
---
+++ firstjavaproject/src/com/dbs/firstjavaproject/Welcome.java
@@ -0,0 +1,8 @@
+package com.dbs.firstjavaproject;
+
+public class Welcome {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+    }
+}
