c262dbfb76cf82fe8f7cd7f2da9df44e29ec9b4f
Suryakant <suryakantsurve@gmail.com>
2025-08-05 12:06:39 +0530

    image

---
+++ mydockerapp/src/main/java/com/dbs/mydockerapp/MydockerappApplication.java
@@ -0,0 +1,13 @@
+package com.dbs.mydockerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MydockerappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MydockerappApplication.class, args);
+	}
+
+}
---
+++ mydockerapp/src/test/java/com/dbs/mydockerapp/MydockerappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbs.mydockerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MydockerappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
